2ef523c60aa7080d89b65245976993f63dc51deb
MrMelk <aretov@live.com>
2022-02-26 21:15:51 +0100

    Further in the tutorial.
    Need to learn more about Lombok and Spring

--- javaBackend/demo/src/main/java/com/example/demo/appuser/AppUser.java
+++ javaBackend/demo/src/main/java/com/example/demo/appuser/AppUser.java
@@ -1,10 +1,16 @@
 package com.example.demo.appuser;
 
+
 import java.util.Collection;
 import java.util.Collections;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -22,13 +28,25 @@ import lombok.Setter;
 @NoArgsConstructor
 
 
-//For database connectivity?
+//specifies the class as an enitity
 @Entity
 
 
 
 public class AppUser implements UserDetails{
-    @Id
+
+    
+    @SequenceGenerator(
+        name = "student_sequence",
+        sequenceName = "student_sequence",
+        allocationSize = 1
+    )
+    @Id     //Primary key specification
+    @GeneratedValue (
+        strategy = GenerationType.SEQUENCE,
+        generator = "student_sequence"
+    )
+
     private Long id;
     private String name;
     private String username;
@@ -49,6 +67,7 @@ public class AppUser implements UserDetails{
     this.username = username;
     this.email = email;
     this.password = password;
+    @Enumerated(EnumType.STRING)//Hva er dette og hvorfor trenger jeg det. Den er en enum men må ha med og får en feil?
     this.appUserRole = appUserRole;
     this.locked = locked;
     this.enabled = enabled;
