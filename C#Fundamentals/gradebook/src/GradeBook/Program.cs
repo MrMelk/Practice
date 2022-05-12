24d8f603e4b6946bf3d1402c69619c90490892fc
MrMelk <aretov@live.com>
2022-05-12 15:35:12 +0200

    wollah brur

---
+++ C#Fundamentals/gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Virker som man kan behandle dette som en python x java typ greie
+// Console.WriteLine("Hello, World!"); //Denne kan vi også bruke nå virker det som
+
+
+//using System; //Trenger tydeligvis ikke denne
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(String[] args) 
+        {
+            Console.WriteLine("Hello Mr. Super Cool!");
+        }
+    }
+}
+
+/*
+int a = 2;
+Console.WriteLine(a);
+ */
