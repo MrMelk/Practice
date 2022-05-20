9388b6d81f401c71259f39d10682f9177063b4ae
MrMelk <54366653+MrMelk@users.noreply.github.com>
2022-05-20 16:26:21 +0200

    frsv
    dersc

---
+++ GloboTicket/TicketsDB.js
@@ -0,0 +1,44 @@
+const globoTickets = [
+    {
+        event: 'Hvit BMW',
+        image: require('./images/hvitBMW.png'),
+        eventID: 'DJ-1',
+        description: 'Den pakistansk julenissen sin slede',
+        shortDescription: 'slede',
+        price: 2000000
+    },
+    {
+        event: 'Blå BMW',
+        image: require('./images/blueBMW.png'),
+        eventID: 'SG-1',
+        description: 'Wollah baja, dette var en fet BMW!',
+        shortDescription: ':o',
+        price: 2500000
+    },
+    {
+        event: 'Rød BMW',
+        image: require('./images/redBMW.png'),
+        eventID: 'GT-1',
+        description: 'Wollah baja, dette var en rå BMW!',
+        shortDescription: '>:)',
+        price: 2750000
+    },
+    {
+        event: 'Regnbue BMW',
+        image: require('./images/rainbowBMW.png'),
+        eventID: 'BT-1',
+        description: 'Wollah baja, det er bra du støtter Pride med en så heftig BMW!',
+        shortDescription: '#Pride',
+        price: 3000000
+    },
+    {
+        event: 'Grønn BMW',
+        image: require('./images/greenBMW.png'),
+        eventID: 'AK-1',
+        description: 'Wollah baja, java green er en flott farge for en BMW! Bra valg!',
+        shortDescription: 'javagreen',
+        price: 1500000
+    }
+];
+
+export default globoTickets;
